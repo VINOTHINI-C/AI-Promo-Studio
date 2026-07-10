033b48dd964b2c94f09b759cab5abcf88e78c020
Vinothini C <vinothinic074@gmail.com>
2026-07-10 23:41:17 +0530

    Added Firebase Login Authentication

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,25 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase/firebase";
 function Login() {
+  const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+
+const navigate = useNavigate();
+const handleLogin = async (e) => {
+  e.preventDefault();
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+
+    alert("Login Successful 🎉");
+
+    navigate("/dashboard");
+  } catch (error) {
+    alert(error.message);
+  }
+};
   return (
     <div className="flex min-h-screen items-center justify-center bg-slate-950 px-6">
 
@@ -12,19 +33,23 @@ function Login() {
           Sign in to continue to AI Promo Studio
         </p>
 
-        <form className="space-y-5">
+        <form className="space-y-5" onSubmit={handleLogin}>
 
           <input
-            type="email"
-            placeholder="Email Address"
-            className="w-full rounded-xl border border-gray-700 bg-transparent px-4 py-3 text-white outline-none focus:border-fuchsia-500"
-          />
+  type="email"
+  placeholder="Email Address"
+  value={email}
+  onChange={(e) => setEmail(e.target.value)}
+  className="w-full rounded-xl border border-gray-700 bg-transparent px-4 py-3 text-white outline-none focus:border-fuchsia-500"
+/>
 
-          <input
-            type="password"
-            placeholder="Password"
-            className="w-full rounded-xl border border-gray-700 bg-transparent px-4 py-3 text-white outline-none focus:border-fuchsia-500"
-          />
+         <input
+  type="password"
+  placeholder="Password"
+  value={password}
+  onChange={(e) => setPassword(e.target.value)}
+  className="w-full rounded-xl border border-gray-700 bg-transparent px-4 py-3 text-white outline-none focus:border-fuchsia-500"
+/>
 
           <button
             className="w-full rounded-xl bg-gradient-to-r from-purple-600 to-pink-600 py-3 font-bold text-white transition hover:scale-105"
