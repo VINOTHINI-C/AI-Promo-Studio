970bd743c39891273fe12f2b2e255635a5892c99
Vinothini C <vinothinic074@gmail.com>
2026-07-10 22:42:17 +0530

    Added Firebase Authentication and Register feature

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,33 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase/firebase";
 
 function Register() {
+  const navigate = useNavigate();
+  const [name, setName] = useState("");
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+const [confirmPassword, setConfirmPassword] = useState("");
+const handleRegister = async (e) => {
+  e.preventDefault();
+
+  if (password !== confirmPassword) {
+    alert("Passwords do not match!");
+    return;
+  }
+
+  try {
+  await createUserWithEmailAndPassword(auth, email, password);
+
+  alert("Account Created Successfully 🎉");
+
+  navigate("/login");
+} catch (error) {
+  alert(error.message);
+}
+};
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-slate-950 px-6">
 
@@ -14,31 +41,40 @@ function Register() {
           Join AI Promo Studio and start creating amazing AI videos.
         </p>
 
-        <form className="space-y-5">
+        <form className="space-y-5" onSubmit={handleRegister}>
+
 
           <input
-            type="text"
-            placeholder="Full Name"
-            className="w-full rounded-xl border border-gray-700 bg-transparent px-4 py-3 text-white outline-none focus:border-fuchsia-500"
-          />
+  type="text"
+  placeholder="Full Name"
+  value={name}
+  onChange={(e) => setName(e.target.value)}
+  className="w-full rounded-xl border border-gray-700 bg-transparent px-4 py-3 text-white outline-none focus:border-fuchsia-500"
+/>
+
+         <input
+  type="email"
+  placeholder="Email Address"
+  value={email}
+  onChange={(e) => setEmail(e.target.value)}
+  className="w-full rounded-xl border border-gray-700 bg-transparent px-4 py-3 text-white outline-none focus:border-fuchsia-500"
+/>
+
+         <input
+  type="password"
+  placeholder="Password"
+  value={password}
+  onChange={(e) => setPassword(e.target.value)}
+  className="w-full rounded-xl border border-gray-700 bg-transparent px-4 py-3 text-white outline-none focus:border-fuchsia-500"
+/>
 
           <input
-            type="email"
-            placeholder="Email Address"
-            className="w-full rounded-xl border border-gray-700 bg-transparent px-4 py-3 text-white outline-none focus:border-fuchsia-500"
-          />
-
-          <input
-            type="password"
-            placeholder="Password"
-            className="w-full rounded-xl border border-gray-700 bg-transparent px-4 py-3 text-white outline-none focus:border-fuchsia-500"
-          />
-
-          <input
-            type="password"
-            placeholder="Confirm Password"
-            className="w-full rounded-xl border border-gray-700 bg-transparent px-4 py-3 text-white outline-none focus:border-fuchsia-500"
-          />
+  type="password"
+  placeholder="Confirm Password"
+  value={confirmPassword}
+  onChange={(e) => setConfirmPassword(e.target.value)}
+  className="w-full rounded-xl border border-gray-700 bg-transparent px-4 py-3 text-white outline-none focus:border-fuchsia-500"
+/>
 
           <button
             className="w-full rounded-xl bg-gradient-to-r from-purple-600 to-pink-600 py-3 font-bold text-white transition hover:scale-105"
