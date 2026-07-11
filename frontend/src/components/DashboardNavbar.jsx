0be0ac03ce3601534209e547a8f3be87e012b4b2
Vinothini C <vinothinic074@gmail.com>
2026-07-11 10:08:01 +0530

    Added Firebase Logout feature

--- frontend/src/components/DashboardNavbar.jsx
+++ frontend/src/components/DashboardNavbar.jsx
@@ -1,4 +1,19 @@
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase/firebase";
+import { useNavigate } from "react-router-dom";
 function DashboardNavbar() {
+  const navigate = useNavigate();
+  const handleLogout = async () => {
+  try {
+    await signOut(auth);
+
+    alert("Logout Successful 👋");
+
+    navigate("/login");
+  } catch (error) {
+    alert(error.message);
+  }
+};
   return (
     <nav className="flex items-center justify-between border-b border-slate-800 bg-slate-900 px-8 py-4">
 
@@ -6,10 +21,12 @@ function DashboardNavbar() {
         🤖 AI Promo Studio
       </h1>
 
-      <button className="rounded-lg bg-red-600 px-4 py-2 font-semibold text-white">
-        Logout
-      </button>
-
+      <button
+  onClick={handleLogout}
+  className="rounded-lg bg-red-600 px-4 py-2 font-semibold text-white hover:bg-red-700"
+>
+  Logout
+</button>
     </nav>
   );
 }
